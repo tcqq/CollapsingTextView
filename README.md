9dc223ecbc2ed74b4d026d983e7944d6e6119997
tcqq <usertcqq@gmail.com>
2019-04-25 01:42:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+[![API](https://img.shields.io/badge/API-17%2B-brightgreen.svg?style=flat)](https://android-arsenal.com/api?level=17)
+[ ![Download](https://api.bintray.com/packages/tcqq/android/collapsingtextview/images/download.svg?version=1.0.0) ](https://bintray.com/tcqq/android/collapsingtextview/1.0.0/link)
+[![Licence](https://img.shields.io/badge/Licence-Apache2-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0)
+
+# CollapsingTextView
+
+CollapsingTextView enables TextView to support displaying more or less text.
+
+# Usage
+Supported attributes with _default_ values:
+``` 
+<com.tcqq.collapsingtextview.CollapsingTextView
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android usual attrs
+    (see below).../>
+```
+|**Attrs**|**Default** |
+|:---|:---|
+| `collapsing_more_text` | `@string/see_less`
+| `collapsing_less_text` | `@string/see_more`
+| `collapsing_text_color` | `?attr/colorPrimary`
+| `collapsing_showing_line` | `1`
+
+# Setup
+#### build.gradle
+```
+repositories {
+    jcenter()
+}
+```
+```
+dependencies {
+    // Using JCenter
+    implementation 'com.tcqq.android:collapsingtextview:1.0.0'
+}
+```
+
+# Screenshots
+
+![See more](/screenshots/see_more.png)
+![See less](/screenshots/see_less.png)
+
+License
+-------
+
+Copyright 2019 Alan Dreamer.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  <http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ collapsingtextview/src/main/java/com/tcqq/collapsingtextview/CollapsingTextView.kt
@@ -0,0 +1,211 @@
+package com.tcqq.collapsingtextview
+
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.annotation.ColorInt
+import androidx.appcompat.widget.AppCompatTextView
+
+class CollapsingTextView
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : AppCompatTextView(context, attrs) {
+
+    private var isCharEnable: Boolean = false
+    private val ellipsis = "..."
+    private var actionTextColor: Int
+    private var mainText: String = ""
+    private var isAlreadySet: Boolean = false
+
+    var showingLine: Int = 1
+        set(value) {
+            if (value != 0) {
+                isCharEnable = false
+                maxLines = value
+                field = value
+            } else {
+                throw Exception("Line Number cannot be <= 0")
+            }
+        }
+    var showingChar: Int = 0
+        set(value) {
+            if (value != 0) {
+                isCharEnable = true
+                field = value
+                addShowMore()
+            } else {
+                throw Exception("Character length cannot be 0")
+            }
+        }
+    var moreText: String
+    var lessText: String
+
+    init {
+        val a = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.CollapsingTextView, 0, 0
+        )
+
+        val moreText = a.getString(R.styleable.CollapsingTextView_collapsing_more_text) ?: DEFAULT_MORE_TEXT
+        val lessText = a.getString(R.styleable.CollapsingTextView_collapsing_less_text) ?: DEFAULT_LESS_TEXT
+        val actionTextColor = a.getColor(R.styleable.CollapsingTextView_collapsing_text_color, primaryColor(context))
+        val showingLine = a.getInteger(R.styleable.CollapsingTextView_collapsing_showing_line, DEFAULT_SHOWING_LINE)
+        a.recycle()
+
+        this.moreText = moreText
+        this.lessText = lessText
+        this.actionTextColor = actionTextColor
+        this.showingLine = showingLine
+
+        addShowMore()
+    }
+
+    @ColorInt
+    private fun primaryColor(context: Context): Int {
+        val value = TypedValue()
+        context.theme.resolveAttribute(R.attr.colorPrimary, value, true)
+        return value.data
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mainText = text.toString()
+    }
+
+    private fun setShowMoreColoringAndClickable() {
+        val spannableString = SpannableString(text)
+
+        Log.d(TAG, "Text: $text")
+        spannableString.setSpan(
+            object : ClickableSpan() {
+                override fun updateDrawState(ds: TextPaint) {
+                    ds.isUnderlineText = false
+                }
+
+                override fun onClick(view: View) {
+                    Log.d(TAG, "展开")
+                    maxLines = Integer.MAX_VALUE
+                    text = mainText
+                    showLess()
+                }
+            },
+            text.length - (moreText.length),
+            text.length, 0
+        )
+
+        spannableString.setSpan(
+            ForegroundColorSpan(actionTextColor),
+            text.length - (moreText.length),
+            text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        movementMethod = LinkMovementMethod.getInstance()
+        setText(spannableString, BufferType.SPANNABLE)
+    }
+
+    private fun addShowMore() {
+        val vto = viewTreeObserver
+        vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val text = text.toString()
+                if (!isAlreadySet) {
+                    mainText = getText().toString()
+                    isAlreadySet = true
+                }
+                val showingText = StringBuilder()
+                if (isCharEnable) {
+                    if (showingChar >= text.length) {
+                        try {
+                            throw Exception("Character count cannot be exceed total line count")
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                    var newText = text.substring(0, showingChar)
+                    newText += ellipsis + moreText
+                    setText(newText)
+                    Log.d(TAG, "Text: $newText")
+                } else {
+                    if (showingLine >= lineCount) {
+                        try {
+                            throw Exception("Line Number cannot be exceed total line count")
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            Log.e(TAG, "Error: " + e.message)
+                        }
+
+                        viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        return
+                    }
+                    var start = 0
+                    var end: Int
+                    for (i in 0 until showingLine) {
+                        end = layout.getLineEnd(i)
+                        showingText.append(text.substring(start, end))
+                        start = end
+                    }
+
+                    var newText = showingText.substring(
+                        0,
+                        showingText.length - (ellipsis.length + moreText.length + MAGIC_NUMBER)
+                    )
+                    Log.d(TAG, "Text: $newText")
+                    Log.d(TAG, "Text: $showingText")
+                    newText += ellipsis + moreText
+
+                    setText(newText)
+                }
+
+                setShowMoreColoringAndClickable()
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
+    }
+
+    private fun showLess() {
+        val text = text.toString() + lessText
+        val spannableString = SpannableString(text)
+
+        spannableString.setSpan(
+            object : ClickableSpan() {
+                override fun updateDrawState(ds: TextPaint) {
+                    ds.isUnderlineText = false
+                }
+
+                override fun onClick(view: View) {
+                    Log.d(TAG, "折叠")
+                    maxLines = showingLine
+                    addShowMore()
+                }
+            },
+            text.length - (lessText.length),
+            text.length, 0
+        )
+
+        spannableString.setSpan(
+            ForegroundColorSpan(actionTextColor),
+            text.length - (lessText.length),
+            text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        movementMethod = LinkMovementMethod.getInstance()
+        setText(spannableString, BufferType.SPANNABLE)
+    }
+
+    companion object {
+        private const val TAG = "ShowMoreTextView"
+
+        private const val MAGIC_NUMBER = 5
+
+        private const val DEFAULT_MORE_TEXT = "See more"
+        private const val DEFAULT_LESS_TEXT = "See less"
+        private const val DEFAULT_SHOWING_LINE = 1
+    }
+}
